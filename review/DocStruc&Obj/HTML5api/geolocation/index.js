ea8ffef87114102ed6593709ad1e04a994860616
Craig Dazey <craigdazey@gmail.com>
2016-06-10 10:43:41 -0400

    mess with the timestamp property

--- review/DocStruc&Obj/HTML5api/geolocation/index.js
+++ review/DocStruc&Obj/HTML5api/geolocation/index.js
@@ -2,13 +2,16 @@ function geoSupport() {
   return 'geolocation' in navigator;
 }
 
-function showLocation(lat, lon) {
+function showLocation(lat, lon, date) {
   document.getElementById('lat').innerHTML = `Lat: ${lat}`;
   document.getElementById('lon').innerHTML = `Lon: ${lon}`;
+
+  var ret = new Date(date).toLocaleString();
+  document.getElementById('date').innerHTML = `Date: ${ret}`;
 }
 
 if(geoSupport()) {
   navigator.geolocation.getCurrentPosition(position => {
-    showLocation(position.coords.latitude, position.coords.longitude);
+    showLocation(position.coords.latitude, position.coords.longitude, position.timestamp);
   });
 }
